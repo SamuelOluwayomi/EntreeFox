da637c16083896a96a04d18408c7473a896dbe8c
Samuel Oluwayomi <samuelfaith500@gmail.com>
2025-09-22 23:28:37 +0100

    Adding follower and following count

--- backend/serializers.py
+++ backend/serializers.py
@@ -21,7 +21,17 @@ class UserListSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'bio', 'profile_pic', 'is_staff', 'is_superuser']
 
 class UserProfileSerializer(serializers.ModelSerializer):
+
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_pic']
+        fields = ['id', 'username', 'email', 'bio', 'profile_pic', 'followers_count', 'following_count']
         read_only_fields = ['id', 'username', 'email']
+
+    def get_followers_count(self, user):
+        return user.followers.count()
+
+    def get_following_count(self, user):
+        return user.following.count()
